3fa54e6c21fb0cf80a437b641361174cb4eba04d
Charlotte Dalenbrook <c.dalenbrook@gmail.com>
2020-04-22 10:52:02 +0200

    about edit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "../components/header";
-
-const Layout = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: 80%;
-`;
+import { Layout } from "../styles";
 
 const Title = styled.h1`
   font-size: 3em;
@@ -19,7 +13,7 @@ const InfoDiv = styled.div`
   background-color: white;
   margin: 10px;
   border-radius: 8px;
-  width: 100%;
+  width: 80%;
   -webkit-box-shadow: 6px 6px 5px 0px rgba(64, 138, 241, 0.55);
   -moz-box-shadow: 6px 6px 5px 0px rgba(64, 138, 241, 0.55);
   box-shadow: 6px 6px 5px 0px rgba(64, 138, 241, 0.55);
